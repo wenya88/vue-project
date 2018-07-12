566c17b25b1196fdfaaa979bad8f746bb62f1cc7
chenyulong <1251900949@qq.com>
2018-07-12 19:54:16 +0800

    update

--- src/router/router.js
+++ src/router/router.js
@@ -226,12 +226,6 @@ export const appRouter = [
                 name: 'contractDetails',
                 title: '合同详情',
                 component: resolve => { require(['@/views/epibol/contractManage/component/contractDetails.vue'], resolve); }
-            }, {
-                path: '/epibol/list',
-                icon: 'ios-paper-outline',
-                name: 'list',
-                title: 'list',
-                component: resolve => { require(['@/views/page/list.vue'], resolve); }
             }
         ]
     }, {
