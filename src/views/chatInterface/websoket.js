aa641b04789f735b53d83e34d4a17e06e25a1b44
chenyulong <1251900949@qq.com>
2018-08-10 17:36:11 +0800

    工作台

--- src/views/chatInterface/websoket.js
+++ src/views/chatInterface/websoket.js
@@ -1,7 +1,10 @@
    import notice from '@/notice/notice.js' // 通知类
    import store  from '@/store/index'
+   import Cookies from 'js-cookie';
+   const wsurl = 'ws:' + Cookies.get('hostName').split(':')[1] + ':8282'
+   console.log('数据', wsurl)
   function connectSocket(msgData) {
-    const wsurl = 'ws://59.111.95.148:8282'
+    // const wsurl = 'ws://59.111.95.148:8282'
     window.webSocket = new WebSocket(wsurl)
     /*建立连接*/
     webSocket.onopen = evt => {
