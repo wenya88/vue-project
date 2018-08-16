e74fc088324cd1ab6479c42d1eceb8a51c3c236e
chenyulong <1251900949@qq.com>
2018-08-16 18:40:44 +0800

    组长工作台

--- src/views/epibol/ProjectChild.vue
+++ src/views/epibol/ProjectChild.vue
@@ -2,16 +2,24 @@
    <div class="task_page_all">
       <div class="task_msg_bx" v-for="(item, index) in list" :key="index" v-if="list.length">
         <div class="task_msg_left">
-           <p class="task_introduce" @click="getTask(item.id)">{{item.task_name}}</p>
+           <div class="task_name">
+             <div class="task_name_title">
+              <p class="task_introduce" @click="getTask(item.id)">{{item.task_name}}</p>
+              <p class="project-name">{{item.project_name}}</p>
+             </div>
+            <div class="task_msg_right">
+              <p class="progress_task_all" v-if="!item.progress"><span class="task_progress_name">{{item.progress_name}}</span><span>{{item.progress}}</span>/<span class="task-total">{{item.total}}</span></p>
+            </div>
+           </div>
            <div class="task_man_day">
-             <p class="task_work_msg">{{item.workDay}}个工作日</p>
-             <p class="task_work_time">{{item.expect_start_time}} ~ {{item.expect_end_time}}</p>
+             <p class="task_work_msg">{{item.workDay}}个工作日({{item.expect_start_time}} ~ {{item.expect_end_time}})</p>
+             <!-- <p class="task_work_time">{{item.expect_start_time}} ~ {{item.expect_end_time}}</p> -->
              <p class="task_work_img"><img :src="item.headimage"/><span>{{item.task_person}}</span></p>
            </div>
          </div>
-        <div class="task_msg_right">
+        <!-- <div class="task_msg_right">
            <p class="progress_task_all"><span class="task_progress_name">{{item.progress_name}}</span><span>{{item.progress}}</span>/<span>{{item.total}}</span></p>
-        </div>
+        </div> -->
      </div>
      <div v-if="!list.length">
        <div class='no_task'>
@@ -46,6 +54,7 @@ export default {
 .task_page_all{
  width: 100%;
  min-height: 100%;
+ padding: 0 1px;
 .no_task{
  width: 200px;
  height: 200px;
@@ -60,38 +69,63 @@ export default {
 }
 .task_msg_bx{
    width: 100%;
-   height: 80px;
-   border-bottom: 1px solid rgb(228,228,228);
-   .task_msg_right{
-      width: 120px;
-      height: 100%;
-      line-height: 80px;
-      text-align: right;
-      float: left;
-      .progress_task_all{
-        font-size: 16px;
-      }
-      .task_progress_name{
-        font-size: 18px;
-          margin-right: 10px;
-      }
-    }
+   height: 120px;
+   border: 1px solid #eef1f2;
+   margin-bottom: 20px;
+   padding:10px;
+  //  background: #fdfdfd;
+   border-radius: 4px;
     .task_msg_left{
-       @task: 120px;
-       width: calc(~"100% - @{task}");
+      //  @task: 120px;
+       width: 100%;
        height: 100%;
        float: left;
+       .task_name{
+        width: 100%;
+        height: 60px;
+        .task_name_title{
+          float: left;
+          @title: 120px;
+          width: calc(~"100% - @{title}");
+          height: 100%;
+          .project-name{
+            color: #bdbdbd;
+            font-size: 14px;
+          }
+        }
+        .task_msg_right{
+          width: 120px;
+          height: 100%;
+          // line-height: 80px;
+          text-align: right;
+          float: left;
+          .progress_task_all{
+            height: 30px;
+            line-height: 30px;
+            font-size: 16px;
+            .task-total{
+              color: #3bceb6;
+            }
+          }
+          .task_progress_name{
+            font-size: 18px;
+            margin-right: 10px;
+            color: #3bceb6;
+          }
+        }
+       }
        .task_introduce{
           width: 100%;
           height: 30px;
+          line-height: 30px;
           font-size: 16px;
           cursor: pointer;
         }
         .task_man_day{
            width: 100%;
-           height: 50px;
-           line-height: 50px;
-           color: rgb(200,200,200);
+           height: 40px;
+           line-height: 40px;
+           color: #bdbdbd;
            .task_work_msg{
               float: left;
             }
@@ -100,12 +134,12 @@ export default {
               margin-left: 20px;
             }
             .task_work_img{
-              float: left;
-              margin-left: 80px;
+              float: right;
+              // margin-left: 80px;
               img{
                 width: 30px;
                 height: 30px;
-                margin-top: 10px;
+                // margin-top: 10px;
                 float: left;
                 border-radius: 50%;
               }
--- src/views/epibol/ProjectWorks.vue
+++ src/views/epibol/ProjectWorks.vue
@@ -1,5 +1,6 @@
 <template>
    <div class="page_group_all">
+     <p class="page_navigation"><span class="navig_icon iconfont icon-ymywenjian-copy"></span>全部项目</p>
       <Row class="page_group_row">
         <Col span='16' class="group_left">
            <div class="group_header">
@@ -13,45 +14,46 @@
                     <p class="group_rank_title">今天结束</p>
                  </div>
                   <div class="group_rank">
-                    <p class="group_rank_num">{{taskAllwangs.delay.length}}</p>
+                    <p class="group_rank_numa">{{taskAllwangs.delay.length}}</p>
                     <p class="group_rank_title">已延期</p>
                  </div>
                   <div class="group_rank">
-                    <p class="group_rank_num">8</p>
+                    <p class="group_rank_numb">8</p>
                     <p class="group_rank_title">关注任务</p>
                  </div>
                   <div class="group_rank">
-                    <p class="group_rank_num">{{taskAllwangs.examine.length}}</p>
+                    <p class="group_rank_numc">{{taskAllwangs.examine.length}}</p>
                     <p class="group_rank_title">待审核</p>
                  </div>
                   <div class="group_rank">
-                    <p class="group_rank_num">{{taskAllwangs.backList.length}}</p>
+                    <p class="group_rank_numd">{{taskAllwangs.backList.length}}</p>
                     <p class="group_rank_title">已反馈</p>
                  </div>
               </div>
            </div>
            <div class="group_left_main">
               <div v-for='(item, index) in groupList' :class="`group_main_msg ${item.backGoup}`" :key='index' @click="getTask(index)">
+                <!-- <div> -->
                  <span class="group_head_title" @click="goHome(item.project_id)">{{item.project_name}}</span>
-                 <span :class="`open_botton iconfont ${item.icon}`" @click="getClose(index)"></span>
+                 <span :class="`open_botton iconfont ${item.icon}`" @click.stop="getClose(index)"></span>
                  <div class="close_box" v-if="!item.isOpen">
                    <div class="close_box_child">
                      <div class="close_box_left">
-                       <Progress :percent="item.speed" :stroke-width="7" hide-info status="success">
+                       <Progress :percent="item.speed" :stroke-width="10" hide-info status="success">
                        </Progress>
                      </div>
                      <div class="close_box_right">
-                       <p class="close_popress_msg">进度</p>
+                       <!-- <p class="close_popress_msg">进度</p> -->
                        <p class="close_popress_num"><span>{{item.upload_file}}</span>/<span>{{item.peroject_file}}</span></p>
                      </div>
                    </div>
-                   <div class="close_box_child">
+                   <div class="close_box_childs">
                      <div class="close_box_left">
-                       <Progress :percent="item.work_now" :stroke-width="10" hide-info status="success">
+                       <Progress :percent="item.work_now" :stroke-width="7" hide-info status="success">
                        </Progress>
                      </div>
                      <div class="close_box_right">
-                       <p class="close_popress_msg">时间</p>
+                       <!-- <p class="close_popress_msg">时间</p> -->
                        <p class="close_popress_num">{{item.work_now}}%</p>
                      </div>
                    </div>
@@ -68,27 +70,50 @@
                      <Tooltip content="客户已反馈" class="group_color_block" :style="`width:${item.cbackNum};background: rgb(255,0,0);`" v-if="item.cback">
                     </Tooltip>
                   </div>
-                  <div class="project_all">
+                   <div class="ul_father">
+                      <ul class="ul_box">
+                        <li>
+                          <p><span class="complete"></span><span>已完成</span></p>
+                        </li>
+                        <li>
+                          <p><span class="doing"></span><span>进行中</span></p>
+                        </li>
+                        <li>
+                          <p><span class="suspend"></span><span>暂停中</span></p>
+                        </li>
+                        <li>
+                          <p><span class="begin"></span><span>未开始</span></p>
+                        </li>
+                      </ul>
+                   </div>
+                   <div class="clearfix propress_box">
+                    <P class="propress_surplus" v-if="item.Surplus < 0">剩余<span class="propress_surplus_time">{{item.Surplus}}</span>个工作日</p>
+                    <p class="propress_surplus" v-if="item.Surplus > 0">延期</p>
+                    <div class="prop-bx">
+                      <Progress :percent="item.work_now" hide-info :stroke-width="5" status="success"/>
+                    </div>
+                  </div>
+                  <div class="project_all clearfix">
                     <Col class="project_name" span= '6'>
-                      <p class="title_name">项目时间</p>
-                      <p class="msg_name">{{item.workDay}}个工作日</p>
+                      <p class="title_name"><span class="iconfont icon-riliwenjian-copy"></span>项目时间</p>
+                      <p class="msg_name"><span class="workday">{{item.workDay}}</span>个工作日</p>
                       <p class="msg_name">{{item.start_time}}~{{item.end_time}}</p>
                     </Col>
                     <Col class="complany_name" span= '6' v-if="!item.inside">
-                      <p class="title_name">甲方公司</p>
+                      <p class="title_name"><span class="iconfont icon-gongwenbaoymy-copy"></span>甲方公司</p>
                       <p class="msg_name">{{item.company.name}}</p>
                       <p class="cpy_person">{{item.company.legal}}</p>
                     </Col>
                     <Col class="tem_person" span= '12'>
-                      <p class="title_name">参与成员</p>
+                      <p class="title_name"><span class="iconfont icon-chengyuan"></span>参与成员</p>
                       <p class="msg_name"><span class="people_name" v-for='(items, idx) in item.person' :key='idx'>{{items}}</span></p>
                     </Col>
                   </div>
-                  <div class="propress_box">
+                  <!-- <div class="propress_box">
                     <P class="propress_surplus" v-if="item.Surplus > 0">剩余<span class="propress_surplus_time">{{item.Surplus}}</span>个工作日</p>
                     <p class="propress_surplus" v-if="item.Surplus < 0">延期</p>
                     <Progress :percent="item.work_now" hide-info :stroke-width="5" status="success"/>
-                  </div>
+                  </div> -->
                </div>
                <!-- 展开样式结束 -->
             </div>
@@ -96,14 +121,14 @@
         </Col>
         <Col span='8' class="tables_all_father">
          <div class="tables_all">
-            <Tabs value="name1">
-              <TabPane :label="label" name="name1">
+            <Tabs value="name1" @on-click="getTab">
+              <TabPane :label="label" name="name1"> 
                <project-task :list="taskChild.endNow"></project-task>
               </TabPane>
               <TabPane :label="delayLabel" name="name2">
                  <project-task :list="taskChild.delay"></project-task>
               </TabPane>
-              <TabPane label="关注" name="name3">
+              <TabPane :label="distList" name="name3">
                 <project-task :list="taskChild.distList"></project-task>
               </TabPane>
               <TabPane :label="examineLabel" name="name4">
@@ -112,9 +137,9 @@
               <TabPane :label="backLabel" name="name5">
                  <project-task :list="taskChild.backList"></project-task>
               </TabPane>
-              <TabPane :label="distLabel" name="name6">
+              <!-- <TabPane :label="distLabel" name="name6">
                  <project-task :list="taskChild.distList"></project-task>
-              </TabPane>
+              </TabPane> -->
             </Tabs>
          </div>
         </Col>
@@ -129,6 +154,7 @@ export default {
     return {
       groupList: [],
       groupNum: 0,
+      tabname: 'name1',
       taskAllwangs: {
         endNow: [],
         delay: [],
@@ -145,10 +171,20 @@ export default {
       label: (h) => {
         // const that = this
         return h('div', [
-          h('span', '今天结束'),
+          h('span', '今日'),
           h('Badge', {
             props: {
               count: this.taskChild.endNow.length
+            }
+          })
+        ])
+      },
+      distList: (h) => {
+         return h('div', [
+          h('span', '关注'),
+          h('Badge', {
+            props: {
+              count: this.taskChild.distList.length
             }
           })
         ])
@@ -183,17 +219,6 @@ export default {
           })
         ])
       },
-      distLabel: (h) => {
-        
-        return h('div', [
-          h('span', '未分配'),
-          h('Badge', {
-            props: {
-              count: this.taskChild.distList.length
-            }
-          })
-        ])
-      }
     }
   },
   components: {
@@ -206,16 +231,44 @@ export default {
   //  this.getTask(0)
   },
   methods:{
+    // tabs 点击
+    getTab (e) {
+      const element = document.getElementsByClassName('ivu-tabs-tabpane')
+      const index = e.split('name')[1]
+      for (let i = 0; i< element.length; i++) {
+        const idxs = i + 1
+        if (idxs == index) {
+          element[i].children[0].setAttribute('style', 'display:block;')
+        } else {
+           element[i].children[0].setAttribute('style', 'display:none;')
+        }
+      }
+      // element.forEach((item, idx) => {
+      //   const idxs = idx + 1
+      //   if (idxs == index) {
+      //     item.setAttribute('style', 'display:block;')
+      //   } else {
+      //      item.setAttribute('style', 'display:none;')
+      //   }
+      // })
+      console.log('点击', index)
+    },
     // 关闭按钮
     getClose (index) {
       const list = this.groupList
-      const isOpen = !list[index].isOpen
-      if (isOpen) {
-        list[index].icon = 'icon-fangxiangshang'
-      } else {
-        list[index].icon = 'icon-fangxiangxia'
-      }
-        list[index].isOpen = isOpen
+      list.forEach((item,idx) => {
+        if (index == idx) {
+          item.isOpen = !item.isOpen
+          if (item.isOpen) {
+           item.icon = 'icon-fangxiangshang'
+          } else {
+           item.icon = 'icon-fangxiangxia'
+          }
+        } else {
+          item.isOpen = false
+          item.icon = 'icon-fangxiangxia'
+        }
+      })
     },
     // 获取组长数据
     getGroup () {
@@ -268,11 +321,11 @@ export default {
             element.cbackNum =  Math.floor(element.cback / element.allFile * 100) + '%'
             element.work_now = work_now
             element.workDay = workDay
-            element.end_time = endWork.days
+            element.end_time = endWork.day
             element.peroject_file = taskList.length
             element.upload_file = taskSun.length
             element.speed =  Math.floor(taskSun.length / taskList.length * 100)
-            element.start_time = startWork.days
+            element.start_time = startWork.day
           });
           this.groupNum = list.length
           this.groupList = list
@@ -287,15 +340,18 @@ export default {
     },
     getTimes (day) {
       const days = day.split(' ')[0].replace(/-/g, '/')
+      const date = days.split('/')[1] + '/' + days.split('/')[2]
       const dayList = {
         times: new Date(days).getTime(),
-        days: days
+        days: date,
+        day: days
       }
       return dayList
     },
     // 获取任务
     getTask (index) {
       const list = this.groupList
+      this.getClose(index)
       list.forEach((item, idx) => {
         if (index === idx) {
           item.backGoup = 'backGoup'
@@ -305,6 +361,9 @@ export default {
       })
       // this.groupList = list
       const taskList = list[index].task_list
+      taskList.forEach(elements => {
+        elements.project_name = list[index].project_name
+      })
       this.taskChild = this.getTaskList(taskList)
       // console.log('项目', this.taskChild)
     },
@@ -344,6 +403,7 @@ export default {
         //   distList.push(element)
         // }
       })
+      this.getTab('name1')
       return {
         endNow: endNow,
         delay: delay,
