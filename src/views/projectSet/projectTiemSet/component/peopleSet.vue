5356bcdc4e84d5a35f169702aa575606a2d186b6
raobin <sctjrb@163.com>
2018-08-23 10:16:06 +0800

    成员

--- src/views/projectSet/projectTiemSet/component/peopleSet.vue
+++ src/views/projectSet/projectTiemSet/component/peopleSet.vue
@@ -1,17 +1,19 @@
 <template>
-    <main class="projectMemberContainer">
-        <section class="member">
-            <p class="title" v-if="memberArray">已加入项目组（<span style="color: red">{{memberArray.length}}</span>）</p>
+    <div class="projectMemberContainer">
+        <div class="member">
+            <p class="title" v-if="memberArray">已加入项目组(<span> {{ memberArray.length }} </span>)</p>
             <ul class="cardList">
                 <template v-if="memberArray">
                     <li class="cardBox" v-for="(item,index) in memberArray" :key="index">
                         <div class="card">
-                            <Icon @click.native="removeShow(item.id)" class="sign" type="close-round"></Icon>
                             <img  :src="item.headimage ? item.headimage : imgHead" alt="头像">
                             <p>{{item.remark_name}}</p>
-                            <div style="color: #a1a1a1">{{item.department_name}}</div>
-                            <p>{{item.job}}</p>
                         </div>
+                        <div class="job">
+                            <span>{{item.department_name}}</span>
+                            <!--<span>{{item.job}}</span>-->
+                        </div>
+                        <i class="deleteI" @click.native="removeShow(item.id)"></i>
                     </li>
                 </template>
                 <Spin v-if="!memberArray" size="large" fix>
@@ -30,20 +32,42 @@
                     说明：如果该成员负责相关任务，移除后任务将暂停，任务执行人将空缺
                 </div>
             </Modal>
-        </section>
-        <section class="whole">
+        </div>
+        <div class="whole">
             <p class="title">公司成员</p>
             <template v-if="items">
+                <div class="departmentList" v-for="(item,index) in items" :key="index">
+                    <p class="wholeTitle">
+                        <span>{{item.department_name}}</span>
+                        <i class="ivu-icon" @click.native="addShow(item.members)"></i>
+
+                        <!--<Icon type="ios-checkmark"></Icon>-->
+                    </p>
+                    <div class="persionList">
+                        <ul>
+                            <li v-for="(member,i) in item.members" :key="i" class="activeSty">
+                                <div>
+                                    <img :src="member.headimage ? member.headimage : imgHead"/>
+                                    <span>{{member.remark_name}}</span>
+                                </div>
+                                <i class="ivu-icon" @click="onCard(member.member_id)"></i>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+
                 <ul v-for="(item,index) in items" :key="index">
                     <li class="wholeTitle">
                         <span>{{item.department_name}}</span>
-                        <Icon style="cursor: pointer" @click.native="addShow(item.members)" type="plus-round"></Icon>
+                        <i class="ivu-icon" @click.native="addShow(item.members)"></i>
                     </li>
                     <div class="wholeList">
                         <p v-for="(member,i) in item.members" :key="i">
-                        <span v-if="!member.is_join" @click="onCard(member.member_id)">
-                            <img :src="member.headimage ? member.headimage : imgHead" alt="">{{member.remark_name}}
-                            <Icon class="icon" type="plus-round"></Icon></span>
+                            <span v-if="!member.is_join" @click="onCard(member.member_id)">
+                                <img :src="member.headimage ? member.headimage : imgHead" alt="">{{member.remark_name}}
+                                <Icon class="icon" type="plus-round"></Icon>
+                            </span>
                             <span v-else @click="onCard(member.member_id)" class="pitchOn">
                                 <img :src="member.headimage ? member.headimage : imgHead" alt="">{{member.remark_name}}
                                 <Icon class="icon" type="checkmark-circled"></Icon>
@@ -52,18 +76,19 @@
                     </div>
                 </ul>
             </template>
-            <Modal
-                    class="removeCard"
-                    v-model="addModal"
-                    width="200"
-                    @on-ok="addGroup"
-                    :closable="false">
-                <div style="color: #FF6600">
-                    部门成员全部加入项目？
-                </div>
-            </Modal>
-        </section>
-    </main>
+        </div>
+    </div>
+
+            <!--<Modal-->
+                    <!--class="removeCard"-->
+                    <!--v-model="addModal"-->
+                    <!--width="200"-->
+                    <!--@on-ok="addGroup"-->
+                    <!--:closable="false">-->
+                <!--<div style="color: #FF6600">-->
+                    <!--部门成员全部加入项目？-->
+                <!--</div>-->
+            <!--</Modal>-->
 </template>
 
 <script>
@@ -187,51 +212,59 @@
 </script>
 
 <style lang="less" scoped>
-    @black: black;
-    @gray: #F2F2F2;
+    @black: #d9d9d9;
+    @gray: #999;
+    @green:#c4f0e9;
+    @ddd:#ddd;
 
     .projectMemberContainer {
         display: flex;
-        height: 100%;
+        justify-content: space-between;
+        padding: 30px;
         .member, .whole {
-            flex: 1;
+            background: #fff;padding: 30px;
             .title {
-                padding: 30px 0 30px 35px;
                 font-size: 16px;
-                color: @black;
-                font-weight: 600;
+                color: @gray;
+                span{color: @green}
             }
         }
         .member {
-            border-right: 1px solid #e4e4e4;
+            width: 35%;
             .cardList {
-                position: relative;
-                height: 100%;
+                margin-top: 30px;
+                display: flex;
+                /*justify-content: space-between;*/
+                flex-wrap: wrap;
+                padding: 2px;
                 .demo-spin-icon-load {
                     animation: ani-demo-spin 1s linear infinite;
                 }
                 .cardBox {
-                    float: left;
-                    width: 25%;
+                    /*float: left;*/
+                    position: relative;
+                    width: 30%;
+                    /*margin-bottom: 20px;*/
+                    border-radius: 5px;
+                    overflow: hidden;
+                    margin: 0 8px 20px 8px;
+                    min-width: 132px;
                     .card {
-                        position: relative;
-                        height: 200px;
-                        padding: 25px 0;
-                        margin: 10px 20px 0 0px;
+                        background: url("./image/autherBG.png") no-repeat;
+                        background-size: 100% 100%;
                         text-align: center;
-                        background: @gray;
-                        border-radius: 4px;
-                        box-shadow: 2px 2px 4px 0 #999;
+                        padding: 30px 0;
                         p {
                             font-size: 14px;
-                            color: @black;
+                            color: #fff;
                         }
                         img {
-                            width: 85px;
-                            height: 85px;
-                            box-shadow: 0 0 2px 0 @black;
-                            border-radius: 50%;
+                            width: 50px;
+                            height: 50px;
+                            border-radius: 100%;
                         }
+
+
                         .sign {
                             display: none;
                             position: absolute;
@@ -252,6 +285,29 @@
                             }
                         }
                     }
+                    .job{
+                        border-radius: 5px;text-align: center;padding: 20px 0;border-left: 1px solid @ddd;border-right: 1px solid @ddd;border-bottom: 1px solid @ddd;
+                        span{
+                            display: inline-block;border: 1px solid orange;color: orange;padding: 2px 5px;font-size: 14px;border-radius: 3px;
+                        }
+                    }
+                    .deleteI{
+                        position: absolute;
+                        top: 0;
+                        right: 0;
+                        display: inline-block;
+                        width: 30px;
+                        height: 30px;
+                        background: url("./image/delectIco.png") no-repeat;
+                        background-position: 7px -44px;
+                    }
+                    &:hover{
+                        cursor: pointer;
+                        .deleteI{
+                            background: url("./image/delectIco.png") no-repeat rgba(0,0,0,.3);
+                            background-position: 7px 7px;
+                        }
+                    }
                 }
                 &::after {
                     content: ' ';
@@ -262,18 +318,56 @@
             }
         }
         .whole {
-            .wholeTitle {
-                display: flex;
-                margin: 0 10px;
-                padding: 4px 25px;
-                font-size: 16px;
-                font-weight: 600;
-                color: @black;
-                background: @gray;
-                border-right: 4px;
-                justify-content: space-between;
-                align-items: center;
+            width: 63%;
+            .departmentList{
+                margin-top: 30px;
+                .wholeTitle {
+                    display: flex;
+                    justify-content: space-between;
+                    padding: 4px 25px;
+                    font-size: 16px;
+                    background: @green;
+                    color: #44ccc0;
+                    padding: 18px 20px;
+                    border-radius: 5px;
+                    min-width: 140px;
+                    i{
+                        display: inline-block;font-style: normal;text-align: center;cursor: pointer;
+                        width: 20px;height: 20px;line-height: 18px;border-radius: 100%;border: 1px solid #44ccc0;
+                        &:after{content: "\F217";color: #44ccc0;font-size: 15px;}
+                    }
+                }
+                .persionList{
+                    ul{
+                        display: flex;flex-wrap: wrap;padding: 0 6px;
+                        li{
+                            display: flex;justify-content: space-between;align-items: center;border: 1px solid #ddd;padding: 10px;min-width: 143px;margin: 30px 20px 0 20px;border-radius: 5px;cursor: pointer;
+                            div{
+                                display: flex;justify-content: center;align-items: center;margin-right: 20px;
+                                img{
+                                    width: 28px;height: 28px;border-radius: 100%;
+                                }
+                            }
+
+                            i{
+                                display: inline-block;font-style: normal;text-align: center;cursor: pointer;
+                                width: 20px;height: 20px;line-height: 18px;border-radius: 100%;border: 1px solid #44ccc0;
+                                &:after{content: "\F217";color: #44ccc0;font-size: 15px;}
+                            }
+                            &.activeSty{
+                                border: 1px solid #44ccc0;
+                                i{
+                                    border: none;
+                                    &:after{content: "\F3FF";color: #44ccc0;font-size: 22px;}
+                                }
+                            }
+                        }
+                    }
+                    /*margin-top: 30px;*/
+                }
             }
+
+
             .wholeList {
                 display: flex;
                 padding: 0 35px 35px 35px;
@@ -287,14 +381,14 @@
                         position: absolute;
                         top: 50%;
                         left: 4px;
-                        width: 20px;
-                        height: 20px;
-                        border-radius: 50%;
+                        width: 28px;
+                        height: 28px;
+                        border-radius: 100%;
                         transform: translateY(-50%);
                     }
                     .pitchOn {
-                        background-color: #18d96c;
-                        box-shadow: 2px 2px 4px 0 #434343;
+                        /*background-color: #18d96c;*/
+                        /*box-shadow: 2px 2px 4px 0 #434343;*/
                         color: #fff;
                         .icon {
                             color: #60ff11;
@@ -308,8 +402,9 @@
                         height: 40px;
                         line-height: 40px;
                         color: #999;
-                        border-radius: 20px;
-                        box-shadow: 0 0 2px 0 #999;
+                        border: 1px solid #ddd;
+                        /*border-radius: 20px;*/
+                        /*box-shadow: 0 0 2px 0 #999;*/
                         cursor: pointer;
                         .icon {
                             position: absolute;
