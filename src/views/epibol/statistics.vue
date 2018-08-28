fdb3d9371a7cbfc54866a51b69226db6e85b8f39
Zhanag <20150106+Zhanag@users.noreply.github.com>
2018-08-28 16:31:47 +0800

    update

--- src/views/epibol/statistics.vue
+++ src/views/epibol/statistics.vue
@@ -58,7 +58,6 @@
     </div>
 </template>
 <script>
-<<<<<<< HEAD
 import statisMenu from './statistics/component/statisMenu';
 import statisHome from './statistics/component/statisHome';
 export default {
@@ -69,68 +68,6 @@ export default {
     },
     components:{
         statisMenu:statisMenu
-=======
-    import mainNative from '../main-components/mainNative.vue';
-    export default {
-        data(){
-            return{
-                menuData:[
-                    {
-                        icon:'icon-zhexiantu',
-                        title:'统计概况',
-                        Titem:[
-                            {title:'概况'}
-                        ]
-                    },
-                    {
-                        icon:'icon-zhaobiao',
-                        title:'投标',
-                        Titem:[
-                            {title:'中标率'},
-                            {title:'落标原因统计'}
-                        ]
-                    },
-                    {
-                        icon:'icon-jiaoyi',
-                        title:'交易',
-                        Titem:[
-                            {title:'成交项目'},
-                            {title:'交易财务'}
-                        ]
-                    },
-                    {
-                        icon:'icon-icon-project',
-                        title:'项目',
-                        Titem:[
-                            {title:'任务资源'},
-                            {title:'通过率'}
-                        ]
-                    },
-                    {
-                        icon:'icon-tuandui1',
-                        title:'团队',
-                        Titem:[
-                            {title:'团队能力'},
-                            {title:'个人能力'},
-                            {title:'工时统计'},
-                            {title:'任务平均用时'},
-                            {title:'类型平均延误用时'},
-                            {title:'稿件通过率'}
-                        ]
-                    }
-
-                ]
-            }
-        },
-        methods:{
-            selectMenu(name){
-                console.log(name)
-            }
-        },
-        components:{
-            mainNative
-        }
->>>>>>> 99e33a5c27de4582e665ebacbe23f30a34b69e32
     }
 }
 </script>
