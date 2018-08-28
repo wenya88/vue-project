cd84465ba840f09cde56344b12dddc77def6e516
Zhanag <20150106+Zhanag@users.noreply.github.com>
2018-08-28 16:30:22 +0800

    update

---
+++ src/views/epibol/contractManage/component/contractDetails/contractDeta.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="contractDeta">
+        <!-- 合同列表 -->
+        <div class="contractList">
+            <p v-for="(item,index) in filesData">
+                <img :src="item.file_url" :class="[nIndex==index?'showImg':'']" @click="showIMG(index)"/>
+                第 {{index+1}} 页
+            </p>
+        </div>
+        <!-- 合同 -->
+        <div class="contract">
+            <img :src="Csrc" />
+        </div>
+        <!-- //END -->
+        <div class="clear"></div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            filesData:Array,
+            Csrc:"",
+            nIndex:0
+        }
+    },
+    mounted(){
+        this.autoH();
+    },
+    methods:{
+         autoH(){
+             let H=document.documentElement.clientHeight;
+             document.getElementsByClassName('contractList')[0].style.height=H-250+'px';
+             document.getElementsByClassName('contract')[0].style.height=H-250+'px';
+         },
+         // 组件数据
+         moduleData(data){
+             this.filesData=data;
+             this.Csrc=data[0].file_url;
+         },
+         showIMG(index){
+            this.nIndex=index;
+            this.Csrc=this.filesData[index].file_url;
+         }
+    }
+}
+</script>
--- src/views/epibol/statistics.vue
+++ src/views/epibol/statistics.vue
@@ -1,8 +1,5 @@
 <template>
     <div class="statisics">
-        <main-native>
-            <div class="main-header-style iconfont"><i></i>{{this.$route.meta.title}}</div>
-        </main-native>
         <!-- Menu -->
         <div class="statisics-menu">
             <statis-menu></statis-menu>
@@ -17,7 +14,6 @@
     </div>
 </template>
 <script>
-<<<<<<< HEAD
 import statisMenu from './statistics/component/statisMenu';
 import statisHome from './statistics/component/statisHome';
 export default {
@@ -27,70 +23,9 @@ export default {
         }
     },
     components:{
-        statisMenu:statisMenu,
-=======
-    import mainNative from '../main-components/mainNative.vue';
-    export default {
-        data(){
-            return{
-                menuData:[
-                    {
-                        icon:'icon-zhexiantu',
-                        title:'统计概况',
-                        Titem:[
-                            {title:'概况'}
-                        ]
-                    },
-                    {
-                        icon:'icon-zhaobiao',
-                        title:'投标',
-                        Titem:[
-                            {title:'中标率'},
-                            {title:'落标原因统计'}
-                        ]
-                    },
-                    {
-                        icon:'icon-jiaoyi',
-                        title:'交易',
-                        Titem:[
-                            {title:'成交项目'},
-                            {title:'交易财务'}
-                        ]
-                    },
-                    {
-                        icon:'icon-icon-project',
-                        title:'项目',
-                        Titem:[
-                            {title:'任务资源'},
-                            {title:'通过率'}
-                        ]
-                    },
-                    {
-                        icon:'icon-tuandui1',
-                        title:'团队',
-                        Titem:[
-                            {title:'团队能力'},
-                            {title:'个人能力'},
-                            {title:'工时统计'},
-                            {title:'任务平均用时'},
-                            {title:'类型平均延误用时'},
-                            {title:'稿件通过率'}
-                        ]
-                    }
-
-                ]
-            }
-        },
-        methods:{
-            selectMenu(name){
-                console.log(name)
-            }
-        },
-        components:{
-            mainNative
-        }
->>>>>>> 34c8467a821355ddf92ba12dc6aa9491119fa4b6
+        statisMenu:statisMenu
     }
+}
 </script>
 <style lang="less">
 @import './statistics/style/statistics.less';
