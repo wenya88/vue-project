ebc9838205fb07a044313cd3a812f5e96d053bba
Zhanag <20150106+Zhanag@users.noreply.github.com>
2018-08-28 16:56:09 +0800

    update

--- src/views/epibol/statistics.vue
+++ src/views/epibol/statistics.vue
@@ -2,56 +2,12 @@
     <div class="statisics">
         <!-- Menu -->
         <div class="statisics-menu">
-            <div class="ivu-div">
-                <Menu :open-names="[1]" width="auto" @on-select="selectMenu" active-name="1-0">
-                    <Submenu :name="(index+1)" v-for="(item,index) in menuData" :key="index">
-                        <template slot="title">
-                            <i :class="`iconfont ${item.icon}`"></i>
-                            {{item.title}}
-                        </template>
-                        <MenuGroup>
-                            <MenuItem :name="(index+1)+'-'+Kindex" v-for="(kitem,Kindex) in item.Titem" :key="Kindex" style="padding-left:70px">{{kitem.title}}</MenuItem>
-                        </MenuGroup>
-                    </Submenu>
-                </Menu>
-            </div>
+            <statis-menu></statis-menu>
         </div>
         <!-- //MenuEnd -->
         <!-- statisicsContent -->
         <div class="statisics-content">
-            <!-- statisInfo -->
-            <div class="statisInfo">
-                <!-- inCome -->
-                <div class="sIncome">
-                    <div class="title">收入</div>
-                    <div>
-                        <div class="infoMent sPayMent">
-                            <i class="iconfont icon-qianbao"></i> 已收款
-                            <p>￥83500.00</p>
-                        </div>
-                        <div class="infoMent sPayAble">
-                            <i class="iconfont icon-shoukuan"></i> 待收款
-                            <p>￥83500.00</p>
-                        </div>
-                        <div class="clear"></div>
-                    </div>
-                </div>
-                <div class="sIncome sDisbu">
-                    <div class="title">支出</div>
-                    <div>
-                        <div class="infoMent outPaid">
-                            <i class="iconfont icon-qiandai"></i> 已支出
-                            <p>￥83500.00</p>
-                        </div>
-                        <div class="infoMent forPaid">
-                            <i class="iconfont icon-zidingyibiaodan-caiwulei_zhichu"></i> 待支出
-                            <p>￥83500.00</p>
-                        </div>
-                        <div class="clear"></div>
-                    </div>
-                </div>
-                <div class="clear"></div>
-            </div>
+            <component :is="contneComponent"></component>
         </div>
         <!-- //statisicsContentEnd -->
         <div class="clear"></div>
@@ -72,7 +28,7 @@ export default {
 }
 </script>
 <style lang="less">
-    @import './statistics/style/statistics.less';
+@import './statistics/style/statistics.less';
 </style>
 
 
